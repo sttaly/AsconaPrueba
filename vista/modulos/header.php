8adb57c0e20e727db962f754b8043bdaa4ccee6d
sttaly <75810068+sttaly@users.noreply.github.com>
2021-04-25 23:23:48 -0500

    Update header.php

--- vista/modulos/header.php
+++ vista/modulos/header.php
@@ -2,8 +2,8 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta name="description" content="sistema escolar">
-    <title>IMPORTANCIONES</title>
+    <meta name="description" content="Prueba">
+    <title>Prueba</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
